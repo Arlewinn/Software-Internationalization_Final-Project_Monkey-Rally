c50b7e07faf0dae1bc3704bdf50cbb0d3c35ee21
rkatya <katyachudinova54@gmail.com>
2021-12-04 21:37:26 -0800

    Test

---
+++ src/assets/content/en/creditsContent.js
@@ -0,0 +1,33 @@
+//Here is the page where we will keep all of the locale specific data for each langauge release. Sections will be separated by the file they are associated with.
+
+//credits.js
+
+var creditText = "Credits:";
+var codeText = "Code:";
+var anitaText = "Anders Anita";
+var graphicText = "Art and Graphics:";
+var erikLauraText = "Erik Nahkala & Laura Kantti";
+var musicText = "Music:";
+var raxlText = "Raxl Snaxel";
+var backText = "Back";
+
+/*var credit = "Credits:";
+var code = "Code:";
+var anita = "Anders Anita";
+var graphic = "Art and Graphics:";
+var erikLaura = "Erik Nahkala & Laura Kantti";
+var music = "Music:";
+var raxl = "Raxl Snaxel";
+var back = "Back";*/
+
+var creditsStyle = {font: "40px CreativeBlock BB", color: 'blue', align: 'center', stroke: '', strokeThickness: 0};
+
+var creditTextX = 500;
+var creditTextY = 350;
+var codeTextX = 500;
+var codeTextY = 300;
+
+
+
+
+
