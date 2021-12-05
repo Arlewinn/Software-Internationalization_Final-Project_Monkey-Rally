98ce418a3066d6f6b57de277c473d195ecfa9f6f
rkatya <katyachudinova54@gmail.com>
2021-12-04 22:46:10 -0800

    I got it to work, sorry for the mess!

--- src/assets/content/en/creditsContent.js
+++ src/assets/content/en/creditsContent.js
@@ -2,30 +2,38 @@
 
 //credits.js
 
+
 var credit = "Credits:";
 var code = "Code:";
-var anita = "Anders Anita";
+var antila = "Anders Antila";
 var graphic = "Art and Graphics:";
 var erikLaura = "Erik Nahkala & Laura Kantti";
 var music = "Music:";
 var raxl = "Raxl Snaxel";
-var back = "Back";
+var backText = "Back";
 
-/*var credit = "Credits:";
-var code = "Code:";
-var anita = "Anders Anita";
-var graphic = "Art and Graphics:";
-var erikLaura = "Erik Nahkala & Laura Kantti";
-var music = "Music:";
-var raxl = "Raxl Snaxel";
-var back = "Back";*/
+var creditsStyle = {font: "95px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle2 = {font: "30px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle3 = {font: "49px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle4 = {font: "40px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
 
-var creditsStyle = {font: "40px CreativeBlock BB", color: 'blue', align: 'center', stroke: '', strokeThickness: 0};
 
-var creditX = 500;
-var creditY = 350;
-var codeX = 500;
-var codeY = 300;
+var creditX = 450;
+var creditY = 48;
+var codeX = 600;
+var codeY = 178;
+var antilaX = 480;
+var antilaY = 218;
+var graphicX = 510;
+var graphicY = 312;
+var erikLauraX = 315;
+var erikLauraY = 354;
+var musicX = 598;
+var musicY = 448;
+var raxlX = 500;
+var raxlY = 488;
+var backTextX = 595;
+var backTextY = 610;
 
 
 
--- src/assets/content/en/levelContent.js
+++ src/assets/content/en/levelContent.js
@@ -36,37 +36,4 @@ var back = "BACK";
 var backX =1002;
 var backY = 580;
 
-// I put this here for now untill I figure out why the game doesn't see the "creditsContent" file I've added for this page.
-
-var credit = "Credits:";
-var code = "Code:";
-var antila = "Anders Antila";
-var graphic = "Art and Graphics:";
-var erikLaura = "Erik Nahkala & Laura Kantti";
-var music = "Music:";
-var raxl = "Raxl Snaxel";
-var backText = "Back";
-
-var creditsStyle = {font: "95px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
-var creditsStyle2 = {font: "30px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
-var creditsStyle3 = {font: "49px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
-var creditsStyle4 = {font: "40px CreativeBlock BB", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
-
-
-var creditX = 450;
-var creditY = 48;
-var codeX = 600;
-var codeY = 178;
-var antilaX = 480;
-var antilaY = 218;
-var graphicX = 510;
-var graphicY = 312;
-var erikLauraX = 315;
-var erikLauraY = 354;
-var musicX = 598;
-var musicY = 448;
-var raxlX = 500;
-var raxlY = 488;
-var backTextX = 595;
-var backTextY = 610;
 
