eb44da332c4264b36d563471124fc45097275d21
ftantussi <francesca.tantussi@gmail.com>
2021-12-05 14:54:06 -0800

    it folder

---
+++ src/assets/content/it/introContent.js
@@ -0,0 +1,31 @@
+//Here is the page where we will keep all of the locale specific data for each langauge release. Sections will be separated by the file they are associated with.
+
+//intro.js
+
+var introTitle = "Monkey\nRally";
+
+var introTitleX = 100;
+var introTitleY = 150;
+
+var introTitleStyle = {font: "150px Unmasked BB", fill: "#0000FF", align: 'center', stroke: '', strokeThickness: 0};
+
+var introText = ["Oh no! C'è stato un incidente in laboratorio\ne sei intrappolato tra le fiamme!",
+"Your army of trained monkeys\n have run away…",
+"…all but one.",
+"one monkey.",
+"one set of directions to give.",
+"save yourself! "];
+var introStyle = {font: "italic 42px Unmasked BB", fill: "#0000FF", align: 'center', stroke: '', strokeThickness: 0};
+var intro1X = 115;
+var intro1Y = 98;
+var intro2X = 330;
+var intro2Y = 130;
+var intro3X = 500;
+var intro3Y = 155;
+var intro4X = 510;
+var intro5X = 340;
+var intro6X = 485;
+
+
+var popupmenubuttontextX = 200;
+var popupmenubuttontextxDissapear = 2000;
