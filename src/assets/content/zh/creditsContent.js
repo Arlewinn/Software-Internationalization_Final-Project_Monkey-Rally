f8cae0ddc94080cf4151195543109591420fce08
Xinxin <450490493@qq.com>
2021-12-08 19:59:11 -0800

    zh translation
    add zh translation in Content file

---
+++ src/assets/content/zh/creditsContent.js
@@ -0,0 +1,41 @@
+//Here is the page where we will keep all of the locale specific data for each langauge release. Sections will be separated by the file they are associated with.
+
+//credits.js
+
+
+var credits = "制作团队：";
+var code = "代码：";
+var antila = "Anders Antila";
+var graphic = "图像：";
+var erikLaura = "Erik Nahkala & Laura Kantti";
+var music = "音乐：";
+var raxl = "Raxl Snaxel";
+var backText = "返回";
+
+var creditsStyle = {font: "95px Hannotate TC", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle2 = {font: "30px Hannotate TC", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle3 = {font: "49px Hannotate TC", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle4 = {font: "40px Hannotate TC", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+
+
+var creditsX = 450;
+var creditsY = 48;
+var codeX = 600;
+var codeY = 178;
+var antilaX = 480;
+var antilaY = 218;
+var graphicX = 510;
+var graphicY = 312;
+var erikLauraX = 315;
+var erikLauraY = 354;
+var musicX = 598;
+var musicY = 448;
+var raxlX = 500;
+var raxlY = 488;
+var backTextX = 595;
+var backTextY = 610;
+
+
+
+
+
