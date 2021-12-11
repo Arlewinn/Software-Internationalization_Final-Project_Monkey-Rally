04e46ffc3c4fa38f7bf58c369812cb22d373219f
rkatya <katyachudinova54@gmail.com>
2021-12-10 18:03:48 -0800

    Adjusted RU

--- src/assets/content/ru/gameContent.js
+++ src/assets/content/ru/gameContent.js
@@ -3,12 +3,12 @@ var unmute = "Выкл. звук";
 var menu = "Меню";
 var backGame = "Назад";
 
-var gameContentStyle = {font: "37px TroubleSide", fill: "#0000FF", align: 'center', stroke: '', strokeThickness: 0};
+var gameContentStyle = {font: "32px TroubleSide", fill: "black", align: 'center', stroke: '', strokeThickness: 0};
 
-var menuX = 588;
-var menuY = 342;
-var backGameX = 588;
-var backGameY = 440;
+var menuX = 585;
+var menuY = 346;
+var backGameX = 585;
+var backGameY = 445;
 var muteX = 588;
 var muteY = 240;
 var unmuteX = 565;
