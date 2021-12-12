cedd5a44be9771280b5b7b393ed987ec82e1c2b2
rkatya <katyachudinova54@gmail.com>
2021-12-11 18:16:35 -0800

    Credits ru

--- src/assets/content/ru/creditsContent.js
+++ src/assets/content/ru/creditsContent.js
@@ -11,29 +11,40 @@ var erikLaura = "Erik Nahkala & Laura Kantti";
 var music = "Музыка:";
 var raxl = "Raxl Snaxel";
 var backText = "Назад";
+var loc = "Локализация:"
+var locName = "Andrew Taylor, Ekaterina Chudinova,\nFrancesca Tantussi, Xinxin Huang, Yijun Wu";
+var thanks = "Особая благодарность twitch.tv/OverlordSeaweaver";
+
 
 var creditsStyle = {font: "80px TroubleSide", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
 var creditsStyle2 = {font: "25px TroubleSide", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
 var creditsStyle3 = {font: "40px TroubleSide", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
 var creditsStyle4 = {font: "32px TroubleSide", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
+var creditsStyle5 = {font: "32px TroubleSide", color: 'black', align: 'center', stroke: '', strokeThickness: 0};
 
 
 var creditsX = 450;
-var creditsY = 48;
+var creditsY = 0;
 var codeX = 600;
-var codeY = 178;
+var codeY = 100;
 var antilaX = 470;
-var antilaY = 218;
-var graphicX = 520;
-var graphicY = 312;
+var antilaY = 140;
+var graphicX = 525;
+var graphicY = 205;
 var erikLauraX = 310;
-var erikLauraY = 354;
+var erikLauraY = 240;
 var musicX = 573;
-var musicY = 448;
+var musicY = 305;
 var raxlX = 490;
-var raxlY = 488;
+var raxlY = 340;
 var backTextX = 587;
 var backTextY = 615;
+var locTextX = 525;
+var locTextY = 400;
+var locNameX = 220;
+var locNameY = 440;
+var thanksX = 170;
+var thanksY = 540;
 
 
 
